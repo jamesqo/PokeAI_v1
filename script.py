4589fef6b006cfc27a70ba78f4bd92333fd6abde
James Ko <jamesqko@gmail.com>
2018-09-04 13:53:49 -0400

    Work

--- script.py
+++ script.py
@@ -4,6 +4,11 @@ from player import Player
 
 sel = MoveSelector()
 # train it...
-game_state = GameState
+game_state = GameState(
+    us=Player(
+        mon_0=Pokemon(
+            )),
+    them=Player(
+        ))
 next_move = sel.predict(game_state)
 next_game_state = game_state.execute(next_move)
